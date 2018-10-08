2d230352bb019219ea802838b8098ef2cc96151c
ManbokLee <dlakdks@nate.com>
2018-10-09 03:18:59 +0900

    ~~ 마이너 코드정리

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -2,65 +2,31 @@
   <!--보상영역 스팀보상 스틱보상-->
   <v-container grid-list-xl>
     <div class="my-profile text-xs-center">
-      <v-avatar
-        size="80"
-        color="grey lighten-4"
-      >
+      <v-avatar size="80" color="grey lighten-4">
         <img :src="me.json_metadata.profile.profile_image" alt="avatar">
       </v-avatar>
-      <div class="my-me-name">
-        {{ me.name }}
-      </div>
+      <div class="my-me-name">{{ me.name }}</div>
       <div class="my-me-about">
         {{ me.json_metadata.profile.about }}
       </div>
-      <v-layout
-        align-center
-        justify-center
-        row
-        class="text-xs-center"
-      >
+      <v-layout align-center justify-center row class="text-xs-center">
         <v-flex xs2 md1>
-          <div class="my-me-follow-count">
-            {{ follow.follower_count }}
-          </div>
-          <div class="my-me-follow-text">
-            팔로워
-          </div>
+          <div class="my-me-follow-count">{{ follow.follower_count }}</div>
+          <div class="my-me-follow-text">팔로워</div>
         </v-flex>
         <v-flex xs2 md1>
-          <div class="my-me-follow-count">
-            {{ follow.following_count }}
-          </div>
-          <div class="my-me-follow-text">
-            팔로잉
-          </div>
+          <div class="my-me-follow-count">{{ follow.following_count }}</div>
+          <div class="my-me-follow-text">팔로잉</div>
         </v-flex>
       </v-layout>
-      <div class="my-me-created">
-        가입일 {{ created }}
-      </div>
+      <div class="my-me-created">가입일 {{ created }}</div>
       <div class="my-me-link">
         <a :href="me.json_metadata.profile.website" target="website">{{ me.json_metadata.profile.website }}</a>
       </div>
       <div>
-        <v-btn
-          dark
-          color="deep-purple"
-        >
-          팔로우
-        </v-btn>
-        <v-btn
-          dark
-          color="light-blue lighten-1"
-        >
-          송금
-        </v-btn>
-        <v-btn
-          color="error"
-        >
-          차단
-        </v-btn>
+        <v-btn dark color="deep-purple">팔로우</v-btn>
+        <v-btn dark color="light-blue lighten-1">송금</v-btn>
+        <v-btn color="error">차단</v-btn>
       </div>
     </div>
 
@@ -72,7 +38,6 @@
         <v-flex  d-inline-block class="item" :class="{'active' : page.midSelect === 'reward'}" @click="setMidMenu('reward')">보상</v-flex>
         <v-flex  d-inline-block class="item" :class="{'active' : page.midSelect === 'wallet'}" @click="setMidMenu('wallet')">지갑</v-flex>
       </v-flex>
-
 
       <!--스티커 관련 영역 -->
       <v-slide-y-transition
@@ -130,12 +95,7 @@
       </v-slide-y-transition>
 
       <!--보상 관련 내용-->
-      <v-slide-y-transition
-        class="py-0"
-        group
-        tag="v-flex"
-        v-if="page.midSelect === 'reward'"
-      >
+      <v-slide-y-transition class="py-0" group tag="v-flex" v-if="page.midSelect === 'reward'">
         <!--스팀보상 스티밋보상 선택 -->
         <v-flex sm12 justify-center class="area-submenu"  transition="slide-y-transition" key="'reward'">
           <v-flex text-xs-left>
@@ -146,12 +106,7 @@
         </v-flex>
 
         <v-list v-if="page.subSelect === 'steemReward' && page.steemRewardList.length > 0" key="'reward-curationReward'">
-          <v-data-table
-            :items="page.steemRewardList"
-            class="elevation-5 table-steem-reward"
-            hide-headers
-            hide-actions
-          >
+          <v-data-table :items="page.steemRewardList" class="elevation-5 table-steem-reward" hide-headers hide-actions>
             <template slot="items" slot-scope="props">
               <td class="reward-time">{{ props.item[1].timestamp | convdate | ago }}</td>
               <td class="reward-title">{{ props.item[1].op[0] === 'curation_reward' ? '큐레이션 보상' : '저자 보상' }}</td>
@@ -173,120 +128,11 @@
 
       </v-slide-y-transition>
 
-
-
     </div>
     <!--My 페이지 종료-->
 
-
-
   </v-container>
 </template>
-
-<style lang="scss" scoped>
-  @import "../colorset.scss";
-  .my-profile {
-  border: 0.5px solid #c3c3c3;
-  padding-top: 15px;
-  padding-bottom: 20px;
-  .my-me-name {
-    font-size: 1.8rem;
-    font-weight: 600;
-    color: #425363;
-  }
-  .my-me-about {
-    font-size: .9rem;
-    color: #989898;
-  }
-  .my-me-follow-count {
-    font-size: 1.5rem;
-    font-weight: 600;
-    color: #425363;
-  }
-  .my-me-follow-text {
-    color: #425363;
-  }
-  .my-me-created {
-    color: #425363;
-  }
-  .my-me-link a {
-    color: #425363;
-  }
-}
-
-  /* 마치페이지 중단 선택 메뉴 */
-  .area-my-mid-menu {
-    .my-mid-menu {
-      border-bottom: 0.5px solid $defaultRightBorderColor;
-      padding-bottom: 0 !important;
-      .item {
-        @extend .basic-item;
-        padding: 12px 12px 11.5px;
-        position: relative;
-        top: 0.5px;
-        &.active {
-          border-bottom: 0.5px solid $colorForce !important;
-        }
-        &:hover {
-          border-bottom: 0.5px solid $colorBeforeForce;
-        }
-      }
-    }
-  }
-
-  /* 컨텐츠 내 메뉴 선택시 생성되는 영역 */
-  .area-submenu {
-    .item {
-      @extend .basic-item;
-      padding: 0.4rem;
-    }
-
-  }
-
-
-  /* 스팀 보상 테이블 */
-  .table-steem-reward {
-    .reward-time {
-      min-width: 8rem;
-    }
-    .reward-title {
-      min-width: 10rem;
-    }
-    .tc-import {
-      color: $colorForce;
-    }
-  }
-
-  /* css class var for extend */
-  .basic-item {
-    transition: 0.2s;
-    cursor: pointer;
-    color: $colorNoneForce;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    &.active {
-      color: $colorForce !important;
-      font-weight: bold;
-    }
-    &:hover {
-      color: $colorBeforeForce;
-      font-weight: bold;
-    }
-  }
-
-  /* In scope all use */
-  .item-separator {
-    color: $colorNoneForce;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-
-</style>
 
 <script>
   import CardMy from '../components/post/CardMy'
@@ -610,3 +456,110 @@
     }
   }
 </script>
+
+
+<style lang="scss" scoped>
+  @import "../colorset.scss";
+  .my-profile {
+    border: 0.5px solid #c3c3c3;
+    padding-top: 15px;
+    padding-bottom: 20px;
+    .my-me-name {
+      font-size: 1.8rem;
+      font-weight: 600;
+      color: #425363;
+    }
+    .my-me-about {
+      font-size: .9rem;
+      color: #989898;
+    }
+    .my-me-follow-count {
+      font-size: 1.5rem;
+      font-weight: 600;
+      color: #425363;
+    }
+    .my-me-follow-text {
+      color: #425363;
+    }
+    .my-me-created {
+      color: #425363;
+    }
+    .my-me-link a {
+      color: #425363;
+    }
+  }
+
+  /* 마치페이지 중단 선택 메뉴 */
+  .area-my-mid-menu {
+    .my-mid-menu {
+      border-bottom: 0.5px solid $defaultRightBorderColor;
+      padding-bottom: 0 !important;
+      .item {
+        @extend .basic-item;
+        padding: 12px 12px 11.5px;
+        position: relative;
+        top: 0.5px;
+        &.active {
+          border-bottom: 0.5px solid $colorForce !important;
+        }
+        &:hover {
+          border-bottom: 0.5px solid $colorBeforeForce;
+        }
+      }
+    }
+  }
+
+  /* 컨텐츠 내 메뉴 선택시 생성되는 영역 */
+  .area-submenu {
+    .item {
+      @extend .basic-item;
+      padding: 0.4rem;
+    }
+
+  }
+
+
+  /* 스팀 보상 테이블 */
+  .table-steem-reward {
+    .reward-time {
+      min-width: 8rem;
+    }
+    .reward-title {
+      min-width: 10rem;
+    }
+    .tc-import {
+      color: $colorForce;
+    }
+  }
+
+  /* css class var for extend */
+  .basic-item {
+    transition: 0.2s;
+    cursor: pointer;
+    color: $colorNoneForce;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    &.active {
+      color: $colorForce !important;
+      font-weight: bold;
+    }
+    &:hover {
+      color: $colorBeforeForce;
+      font-weight: bold;
+    }
+  }
+
+  /* In scope all use */
+  .item-separator {
+    color: $colorNoneForce;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+</style>
+
