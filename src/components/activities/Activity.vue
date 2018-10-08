6481e531c0907a8b5d6fc1edccf8ccbee6648e67
ManbokLee <dlakdks@nate.com>
2018-10-09 04:31:59 +0900

    내활동 페이지 작업분 업데이트

--- src/components/activities/Activity.vue
+++ src/components/activities/Activity.vue
@@ -1,48 +1,61 @@
 <template>
-  <v-card>
-    <v-card-title>
-      {{ type }}
-      <div v-if="type === 'author_reward'">
-       작성하신 <a :href="permlink(item.author, item.permlink)">게시글</a>의 보상으로 <span class="font-weight-bold">{{ item.sbd_payout }} SBD, {{ item.steem_payout }} STEEM, {{ sp(item.vesting_payout) }} SP</span>를 지급받았습니다.
-      </div>
-      <div v-if="type === 'curation_reward'">
-        <span class="font-weight-bold">{{ item.comment_author }}</span>님의 <a :href="permlink(item.comment_author, item.comment_permlink)">게시글</a>의 큐레이션 보상은 <span class="font-weight-bold">{{ sp(item.reward) }} SP</span> 입니다.
-      </div>
-      <div v-if="type === 'vote'">
-        <span class="font-weight-bold">{{ item.author }}</span>님의 <a :href="permlink(item.author, item.permlink)">게시글</a>에 {{ item.weight | vp }}% 보팅하셨습니다.
-      </div>
-      <div v-if="type === 'claim_reward_balance'">
-        <span class="font-weight-bold">{{ item.reward_sbd }} SBD, {{ item.reward_steem }} STEEM, {{ sp(item.reward_vests) }} SP</span>를 내 지갑으로 옮겼습니다.
-      </div>
-      <div v-if="type === 'comment'">
-        <div v-if="item.author === this.$store.state.username">
-          <span class="font-weight-bold">{{ item.parent_author }}</span>님에게 코멘트를 남겼습니다.
-        </div>
-        <div v-else>
-          <span class="font-weight-bold">{{ item.author }}</span>님이 코멘트를 남겼습니다.
-        </div>
-      </div>
-      <div v-if="type === 'transfer'">
-        <div v-if="item.to === this.$store.state.username">
-          <span class="font-weight-bold">{{ item.from }}</span>님이 <span class="font-weight-bold">{{ item.amount }}</span>을 송금하셨습니다.
-        </div>
-        <div v-else>
-          <span class="font-weight-bold">{{ item.to }}</span>님에게 <span class="font-weight-bold">{{ item.amount }}</span>을 송금하셨습니다.
-        </div>
-      </div>
-      <div v-if="type === 'transfer_to_vesting'">
-        <span class="font-weight-bold">{{ item.amount }}</span>만큼 스팀파워업 하였습니다.
-      </div>
-      <div v-if="type === 'custom_json'">
-        <div v-if="json.op === 'follow'">
-          <span class="font-weight-bold">{{ json.data.following }}</span>님의 {{ json.data.what[0] }}을/를 팔로우합니다.
-        </div>
-      </div>
-    </v-card-title>
-    <v-card-text>
-      {{ data.timestamp | convdate | ago }}
-    </v-card-text>
-  </v-card>
+  <v-layout align-center>
+    <div>
+      <v-avatar size="40">
+        <v-img :src="'https://steemitimages.com/u/' + inAuthor + '/avatar/small'"></v-img>
+      </v-avatar>
+    </div>
+    <div>
+      <v-flex>
+        <!--{{ type }}-->
+        <v-flex xs12 v-if="type === 'author_reward'" class="pb-1">
+          작성하신 <a :href="permlink(item.author, item.permlink)">게시글</a>의 보상으로 <span class="font-weight-bold">{{ item.sbd_payout }} SBD, {{ item.steem_payout }} STEEM, {{ sp(item.vesting_payout) }} SP</span>를 지급받았습니다.
+        </v-flex>
+        <v-flex xs12 v-if="type === 'curation_reward'" class="pb-1">
+          <span class="font-weight-bold">{{ item.comment_author }}</span>님의 <a :href="permlink(item.comment_author, item.comment_permlink)">게시글</a>의 큐레이션 보상은 <span class="font-weight-bold">{{ sp(item.reward) }} SP</span> 입니다.
+        </v-flex>
+        <v-flex xs12 v-if="type === 'vote'" class="pb-1">
+          <span class="font-weight-bold">{{ item.author }}</span>님의 <a :href="permlink(item.author, item.permlink)">게시글</a>에 {{ item.weight | vp }}% 보팅하셨습니다.
+        </v-flex>
+        <v-flex xs12 v-if="type === 'claim_reward_balance'" class="pb-1">
+          <span class="font-weight-bold">{{ item.reward_sbd }} SBD, {{ item.reward_steem }} STEEM, {{ sp(item.reward_vests) }} SP</span>를 내 지갑으로 옮겼습니다.
+        </v-flex>
+        <v-flex xs12 v-if="type === 'comment'" class="pb-1">
+          <div v-if="item.author === this.$store.state.username && item.parent_author !== ''">
+            <span class="font-weight-bold">{{ item.parent_author }}</span>님에게 코멘트를 남겼습니다.
+          </div>
+          <div v-if="item.author === this.$store.state.username && item.parent_author === ''"></div>
+            <span> <a :href="permlink(item.author, item.permlink)">게시글</a>을 작성/수정 했습니다.</span>
+          <div v-if="item.author !== this.$store.state.username">
+            <span class="font-weight-bold">{{ item.author }}</span>님이 코멘트를 남겼습니다.
+          </div>
+        </v-flex>
+        <v-flex xs12 v-if="type === 'transfer'" class="pb-1">
+          <div v-if="item.to === this.$store.state.username">
+            <span class="font-weight-bold">{{ item.from }}</span>님이 <span class="font-weight-bold">{{ item.amount }}</span>을 송금하셨습니다.
+          </div>
+          <div v-else>
+            <span class="font-weight-bold">{{ item.to }}</span>님에게 <span class="font-weight-bold">{{ item.amount }}</span>을 송금하셨습니다.
+          </div>
+        </v-flex>
+        <v-flex xs12 v-if="type === 'transfer_to_vesting'" class="pb-1">
+          <span class="font-weight-bold">{{ item.amount }}</span>만큼 스팀파워업 하였습니다.
+        </v-flex>
+        <v-flex xs12 v-if="type === 'custom_json'" class="pb-1">
+          <div v-if="json.op === 'follow' && json.data.what[0]">
+            <span class="font-weight-bold">{{ json.data.following }}</span>님의 {{ json.data.what[0] }}을/를 팔로우합니다.
+          </div>
+          <div v-else>
+            <span class="font-weight-bold">{{ json.data.following }}</span>님을 언팔로우합니다.
+          </div>
+        </v-flex>
+
+        <v-flex xs12 class="pt-0 activity-time">
+          {{ data.timestamp | convdate | ago }}
+        </v-flex>
+      </v-flex>
+    </div>
+  </v-layout>
 </template>
 
 <script>
@@ -76,25 +89,45 @@ export default {
     }
   },
   mounted () {
-    this.$nextTick(function () {
-      this.item = this.data.op[1]
-
-      const testOps = []
-      if (testOps.indexOf(this.type) !== -1) {
-        console.log(this.type, this.data)
-      }
-      const customOp = ['follow']
-      if (this.type === 'custom_json' && customOp.indexOf(this.item.id) === -1) {
-        console.log('custom_json', this.item.id)
-      }
-      if (this.type === 'custom_json') {
-        const tmp = JSON.parse(this.item.json)
-        this.json.op = this.item.id
-        this.json.data = tmp[1]
-      }
-    })
+    this.item = this.data.op[1]
+    // const testOps = []
+    // if (testOps.indexOf(this.type) !== -1) {
+    //   console.log(this.type, this.data)
+    // }
+    // const customOp = ['follow']
+    // if (this.type === 'custom_json' && customOp.indexOf(this.item.id) === -1) {
+    //   console.log('custom_json', this.item.id)
+    // }
+    if (this.type === 'custom_json') {
+      const tmp = JSON.parse(this.item.json)
+      this.json.op = this.item.id
+      this.json.data = tmp[1]
+    }
   },
   computed: {
+    inAuthor: function () {
+      switch (this.type) {
+        case 'vote':
+        case 'author_reward':
+          return this.item.author
+        case 'curation_reward':
+          return this.item.comment_author
+        case 'comment':
+          if (this.item.to === this.$store.state.username) {
+            return this.item.from
+          } else {
+            return this.item.to
+          }
+        case 'transfer_to_vesting':
+          return this.$store.state.username
+        case 'custom_json':
+          if (this.json.op === 'follow') {
+            return this.json.data.following
+          } else {
+            return this.json.data.follower
+          }
+      }
+    }
   },
   methods: {
     sp: function (vests) {
@@ -106,3 +139,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  @import "../../colorset";
+
+  .activity-time {
+    color: $colorNoneForce;
+  }
+
+</style>
--- src/pages/Activities.vue
+++ src/pages/Activities.vue
@@ -5,10 +5,19 @@
         <v-btn>출석체크</v-btn>
       </v-flex>
     </v-layout>
-    <div v-for="(item, i) in history">
-      <activity :type="item[1].op[0]" :data="item[1]"></activity>
-    </div>
-    <v-btn :disabled="limit === -1" @click="fetchBlog()">more</v-btn>
+    <v-data-table :items="history" class="elevation-5" hide-headers hide-actions v-if="history.length > 0">
+      <template slot="items" slot-scope="props">
+        <td><activity :type="props.item[1].op[0]" :data="props.item[1]"></activity></td>
+      </template>
+    </v-data-table>
+    <infinite-loading @infinite="infiniteHandler" v-if="ableLoading && !isLoading"></infinite-loading>
+    <v-flex xs12 justify-center text-xs-center v-if="ableLoading && isLoading">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </v-flex>
+    <!--<div v-for="(item, i) in history">-->
+      <!--<activity :type="item[1].op[0]" :data="item[1]"></activity>-->
+    <!--</div>-->
+    <!--<v-btn :disabled="limit === -1" @click="fetchBlog()">more</v-btn>-->
   </v-container>
 </template>
 
@@ -18,8 +27,14 @@
 <script>
 import steem from '@/services/steem'
 import Activity from '@/components/activities/Activity'
+import InfiniteLoading from 'vue-infinite-loading'
 
 export default {
+  name: 'Activities',
+  components: {
+    Activity,
+    InfiniteLoading
+  },
   data () {
     return {
       history: [],
@@ -27,32 +42,48 @@ export default {
       limit: 29,
       allowed: [
         'vote', 'curation_reward', 'author_reward', 'claim_reward_balance', 'comment', 'transfer', 'transfer_to_vesting', 'custom_json'
-      ]
+      ],
+      ableLoading: true,
+      isLoading: false
     }
   },
-  components: {
-    Activity
-  },
   mounted () {
+    this.isLoading = true
     this.fetchBlog()
   },
   methods: {
     fetchBlog: function () {
       let vm = this
       steem.api.getAccountHistory('smtion', this.lastId, this.limit, function (err, result) {
-        if (err) {}
+        if (err) {
+          vm.ableLoading = false
+          return
+        }
         vm.lastId = result[0][0] - 1
         if (vm.lastId < vm.limit) vm.limit = vm.lastId
         // console.log(vm.lastId, vm.limit)
         result = result.filter(item => {
-          const isAllowed = vm.allowed.indexOf(item[1].op[0]) !== -1
-          if (!isAllowed) {
-            console.info(item[1].op[0])
-          }
-          return isAllowed
+          return vm.allowed.indexOf(item[1].op[0]) !== -1
+          // const isAllowed = vm.allowed.indexOf(item[1].op[0]) !== -1
+          // if (!isAllowed) {
+          //   console.info(item[1].op[0])
+          // }
+          // return isAllowed
         })
         vm.history = vm.history.concat(result.slice().reverse())
+        vm.isLoading = false
       })
+    },
+    infiniteHandler: async function ($state) {
+      if (!this.isLoading) {
+        this.isLoading = true
+        await this.fetchBlog()
+        $state.loaded()
+      } else {
+        $state.loaded()
+        console.log('call infiny other')
+        this.ableLoading = false
+      }
     }
   }
 }
