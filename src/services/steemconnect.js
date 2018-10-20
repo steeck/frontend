fc31e022cbf127693ed1d1bb9a4e43d3ae3a7e57
ManbokLee <dlakdks@nate.com>
2018-10-20 18:45:06 +0900

    피드카드의 보팅 기능 분리

--- src/services/steemconnect.js
+++ src/services/steemconnect.js
@@ -2,7 +2,7 @@ import sc2 from 'steemconnect'
 
 const steemconnect = sc2.Initialize({
   app: 'app.steeck',
-  callbackURL: process.env.APP_HOST + '/#/login',
+  callbackURL: process.env.APP_HOST + '/login',
   accessToken: 'access_token',
   scope: ['vote', 'comment', 'delete_comment', 'comment_options', 'custom_json', 'claim_reward_balance', 'login', 'offline']
 })
