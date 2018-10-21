377f5eb8001bb40cc5b742faae86921f120d3780
Soomin Park <smtion@gmail.com>
2018-10-21 23:35:01 +0900

    setup env

--- src/services/http.js
+++ src/services/http.js
@@ -1,4 +1,3 @@
 import axios from 'axios'
 
-export default axios.create({baseURL: `http://localhost:8081`})
-// export default axios.create({baseURL: `https://api.steeck.io`})
+export default axios.create({ baseURL: process.env.API_HOST })
