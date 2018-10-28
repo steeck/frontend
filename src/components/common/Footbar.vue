4a55cb7bf17d57bac3c493d13d95cf0a3dd217f1
ManbokLee <dlakdks@nate.com>
2018-10-28 14:24:39 +0900

    푸터제거 및 푸터영역에 top 버튼 추가

--- src/components/common/Footbar.vue
+++ src/components/common/Footbar.vue
@@ -1,51 +1,36 @@
 <template>
-  <v-footer
-    height="auto"
-    class="grey lighten-2"
-  >
-    <v-container grid-list-xl>
-      <v-card
-        flat
-        tile
-        class="grey lighten-2"
-      >
-        <v-card-text>
-          <ul>
-            <li><a href="https://about.steeck.io" target="about-steeck">스틱소개</a></li>
-            <li><a href="mailto:support@steeck.io">고객센터</a></li>
-            <li>공식채널</li>
-            <li><a href="mailto:support@steeck.io">고객지원</a></li>
-            <li><a href="mailto:support@steeck.io">제휴문의</a></li>
-          </ul>
-          <ul class="mt-2">
-            <li>약관</li>
-            <li>개인정보 처리방침</li>
-          </ul>
-          <div class="mt-2">
-            &copy;2018 — <strong>STEECK</strong>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-container>
+  <v-footer height="auto" color="white" v-scroll="onScroll">
+    <v-fab-transition>
+      <v-btn fab dark bottom right fixed color="primary" v-show="isBtnView" @click="scrollToTop">
+        <v-icon dark>keyboard_arrow_up</v-icon>
+      </v-btn>
+    </v-fab-transition>
   </v-footer>
 </template>
 
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-}
-li + li:before {
-  content: '|';
-  width: 20px;
-  display: inline-block;
-  text-align: center;
-}
-li a {
-  text-decoration: none;
-  color: inherit;
-}
-</style>
+<script>
+  export default {
+    data: function () {
+      return {
+        offsetTop: 0
+      }
+    },
+    computed: {
+      isBtnView: function () {
+        return this.offsetTop > 10
+      }
+    },
+    methods: {
+      onScroll: function (e) {
+        this.offsetTop = window.pageYOffset || document.documentElement.scrollTop
+      },
+      scrollToTop: function () {
+        window.scroll({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        })
+      }
+    }
+  }
+</script>
